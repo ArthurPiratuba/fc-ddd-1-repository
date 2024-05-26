afc30313a0c1bf3a10d4c2c9634e15f655542121
Arthur Samuel Hack <arthur.hack.live@live.com>
2024-05-25 21:44:42 -0300

    Removed unused entity getters

--- src/domain/customer/address.ts
+++ src/domain/customer/address.ts
@@ -1,42 +1,21 @@
 export default class Address {
-
-  _street: string = "";
-  _number: number = 0;
-  _zip: string = "";
-  _city: string = "";
-
-  constructor(street: string, number: number, zip: string, city: string) {
-    this._street = street;
-    this._number = number;
-    this._zip = zip;
-    this._city = city;
+  constructor(
+    readonly street: string,
+    readonly number: number,
+    readonly zip: string,
+    readonly city: string
+  ) {
     this.validate();
   }
 
-  get street(): string {
-    return this._street;
-  }
-
-  get number(): number {
-    return this._number;
-  }
-
-  get zip(): string {
-    return this._zip;
-  }
-
-  get city(): string {
-    return this._city;
-  }
-
   validate() {
-    if (this._street.length === 0) throw new Error("Street is required");
-    if (this._number === 0) throw new Error("Number is required");
-    if (this._zip.length === 0) throw new Error("Zip is required");
-    if (this._city.length === 0) throw new Error("City is required");
+    if (this.street.length === 0) throw new Error("Street is required");
+    if (this.number === 0) throw new Error("Number is required");
+    if (this.zip.length === 0) throw new Error("Zip is required");
+    if (this.city.length === 0) throw new Error("City is required");
   }
 
   toString() {
-    return `${this._street}, ${this._number}, ${this._zip} ${this._city}`;
+    return `${this.street}, ${this.number}, ${this.zip} ${this.city}`;
   }
 }
